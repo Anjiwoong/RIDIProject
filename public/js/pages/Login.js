b5699473f1a222177dd5775fff584237621c4949
Woong <rlqmatk@naver.com>
2022-08-23 10:09:05 +0900

    login branch merge

--- public/js/pages/Login.js
+++ public/js/pages/Login.js
@@ -1,20 +1,74 @@
-import { createElement, fetchData } from '../app.js';
+import { createElement } from '../app.js';
 
-const Login = async (params, title) => {
-  const data = await fetchData('/data/webtoon.json');
+// 스페이스 방지
+const returnsSpace = e => {
+  if (!e.target.matches('input')) return;
+  if (e.keyCode === 32) e.returnValue = false;
+};
+
+const validate = $loginErrorMessage => {
+  const idValue = document.querySelector('.user-id').value.trim();
+  const pwValue = document.querySelector('.user-pw').value.trim();
+
+  if (!idValue) {
+    $loginErrorMessage.textContent = '! 아이디를 입력해주세요.';
+    return false;
+  }
+  if (!pwValue) {
+    $loginErrorMessage.textContent = '! 비밀번호를 입력해주세요.';
+    return false;
+  }
+
+  return true;
+};
+
+const request = async e => {
+  e.preventDefault();
+
+  const $signinForm = document.querySelector('.login-form');
+  const $loginErrorMessage = document.querySelector('.login__error-message');
+
+  if (!validate($loginErrorMessage)) return;
+
+  const payload = [...new FormData($signinForm)].reduce(
+    // eslint-disable-next-line no-return-assign, no-sequences
+    (obj, [key, value]) => ((obj[key] = value), obj),
+    {}
+  );
+
+  try {
+    const { data: user } = await axios.post(`/login`, payload);
+    console.log('😀 LOGIN SUCCESS!');
+    console.log(user);
+
+    if (user) window.location.href = '/';
+  } catch (e) {
+    console.log('😱 LOGIN FAILURE..');
+    $loginErrorMessage.textContent = '! 아이디 또는 비밀번호를 확인해주세요.';
+  }
+};
+
+const loginEventBinding = () => {
+  const $root = document.getElementById('root');
+  $root.addEventListener('keydown', returnsSpace);
+  $root.addEventListener('submit', request);
+};
+
+const Login = () => {
+  loginEventBinding();
 
   return createElement(`
   <div class="login">
     <div class="login__header">
       <h1>
-        <a href="/" class="link-mainpage">RIDI</a>
+        <a href="#" class="link-mainpage">RIDI</a>
       </h1>
     </div>
     <form class="login-form" method="POST">
       <fieldset>
         <legend>로그인</legend>
-        <input type="text" class="user-id" placeholder="아이디" />
-        <input type="password" class="user-pw" placeholder="비밀번호" />
+        <input type="text" name="userid" class="user-id" placeholder="아이디" />
+        <input type="password" name="password" class="user-pw" placeholder="비밀번호" />
         <div class="login__info">
           <div class="login__info__left">
             <input type="checkbox" class="login-check" id="login-chk" />
@@ -28,12 +82,13 @@ const Login = async (params, title) => {
             <span role="button" class="search-pw">비밀번호 재설정</span>
           </div>
         </div>
+        <span class="login__error-message"></span>
         <button class="login-btn">로그인</button>
-        <a href="/signup">
+        </fieldset>
+        </form>
+        <a href="/signup" class="login__signup-link">
           <button class="signin-btn">회원가입</button>
         </a>
-      </fieldset>
-    </form>
   </div>
   `);
 };
--- server.js
+++ server.js
@@ -1,17 +1,68 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const users = require('./fake-data/users');
+
+require('dotenv').config();
 
 const app = express();
-const port = 8081;
+const PORT = process.env.PORT || 8800;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(cookieParser());
+
+const auth = (req, res, next) => {
+  const { accessToken } = req.cookies;
+  try {
+    const decoded = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
+    console.log(`😀 사용자 인증 성공`, decoded);
+    next();
+  } catch (e) {
+    console.log(req.path);
+    console.error('😱 사용자 인증 실패..', e);
+    if (req.path !== '/login') {
+      res.redirect('/login');
+    }
+    next();
+  }
+};
 
 // 브라우저 새로고침을 위한 처리 (다른 route가 존재하는 경우 맨 아래에 위치해야 한다)
 // 브라우저 새로고침 시 서버는 index.html을 전달하고 클라이언트는 window.location.pathname를 참조해 다시 라우팅한다.
-app.get('*', (req, res) => {
+
+app.get('*', auth, (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on http:/localhost:${port}`);
+app.post('/login', (req, res) => {
+  const { userid, password } = req.body;
+
+  // 401 Unauthorized
+  if (!userid || !password)
+    return res.status(401).send({ error: '사용자 아이디 또는 패스워드가 전달되지 않았습니다.' });
+
+  const user = users.findUser(userid, password);
+  console.log('사용자 정보:', user);
+
+  // 401 Unauthorized
+  if (!user) return res.status(401).send({ error: '등록되지 않은 사용자입니다.' });
+
+  // 토큰 생성
+  const accessToken = jwt.sign({ userid }, process.env.JWT_SECRET_KEY, {
+    expiresIn: '20s',
+  });
+
+  res.cookie('accessToken', accessToken, {
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 7d
+    httpOnly: true,
+  });
+
+  // 로그인 성공
+  res.send({ userid, username: user.name });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http:/localhost:${PORT}`);
 });
