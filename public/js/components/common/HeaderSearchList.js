af559638c28cfd099fa2b1ff49f8ab3d8ef6d570
Woong <rlqmatk@naver.com>
2022-08-27 15:47:08 +0900

    메인페이지 검색기능 완성

---
+++ public/js/components/common/HeaderSearchList.js
@@ -0,0 +1,9 @@
+const HeaderSearchList = () => {
+  document.querySelector('.header-down__form__search__container').innerHTML = `
+  <div class="header-down__form__search__books">
+    <span>작품 이름을 검색해주세요.</span>
+  </div>
+`;
+};
+
+export default HeaderSearchList;
