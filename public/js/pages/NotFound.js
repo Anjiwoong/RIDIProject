a3278ab07333dbdaeea9eb9635764c2a773955b9
rlorxl <imjoeun08@naver.com>
2022-08-31 15:54:54 +0900

    notfound이미지 추가, 메인페이지 캐러셀 변수명 정리

--- public/js/pages/NotFound.js
+++ public/js/pages/NotFound.js
@@ -1,5 +1,11 @@
 import { createElement } from '../app.js';
 
-const NotFound = () => createElement('<h1>미구현 상태</p>');
+const NotFound = () =>
+  createElement(`
+    <div class="notFound-image">
+      <h1 class="sr-only">미구현 페이지</h1>
+      <img src="/assets/notFound/pixeltaku.png"/>
+    </div>
+`);
 
 export default NotFound;
